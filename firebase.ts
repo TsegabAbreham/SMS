110daa879ff323922a33d23c908b1fe9b41dd0fe
TsegabAbreham <tsegabmabreham@gmail.com>
2025-12-29 15:18:11 +0300

    ENVed API keys for security

--- firebase.ts
+++ firebase.ts
@@ -1,22 +1,21 @@
-// firebase.ts
 import { initializeApp } from "firebase/app";
-import { getAuth, Auth } from "firebase/auth";
-import { getFirestore, Firestore } from "firebase/firestore";
-import { getStorage, FirebaseStorage } from "firebase/storage";
+import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
+import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyAXVHOwB3gC2wrl0LdFTq1C6jqlLjDPr5E",
-  authDomain: "sms-school-cddaa.firebaseapp.com",
-  projectId: "sms-school-cddaa",
-  storageBucket: "sms-school-cddaa.appspot.com",
-  messagingSenderId: "220306011200",
-  appId: "1:220306011200:web:4b4dd9cd6ecc214508dba5",
-  measurementId: "G-Y2Z583K4T3"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID,
 };
 
 const app = initializeApp(firebaseConfig);
-const auth: Auth = getAuth(app);
-const db: Firestore = getFirestore(app);
-const storage: FirebaseStorage = getStorage(app);
+const auth = getAuth(app);
+const db = getFirestore(app);
+const storage = getStorage(app);
 
 export { app, auth, db, storage };
